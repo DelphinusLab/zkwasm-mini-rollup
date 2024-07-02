4b60ba9fd09bebe22ddffd14e6bd3eb2dd2f13c7
Glen <97083083+glenzk@users.noreply.github.com>
2024-07-02 18:25:50 +1200

    Add get merkle function from contract (#16)
    Signed-off-by: Glen <glen@zkcross.org>

--- ts/src/settle.ts
+++ ts/src/settle.ts
@@ -1,47 +1,55 @@
 import BN from "bn.js";
 import { ZKWasmAppRpc } from "./rpc.js";
 import {LeHexBN} from "./sign.js";
-import ethers from "ethers";
-import { user_addr } from "./config.js";
+import { ethers } from "ethers";
+import { user_addr, contract_addr, priv } from "./config.js";
+import abiData from './Proxy.json' assert { type: 'json' };
 
 // Replace with your network configuration
 const provider = new ethers.JsonRpcProvider("https://rpc.sepolia.org");
-const signer = provider.getSigner(); // Ensure this signer has the necessary permissions
+const signer = new ethers.Wallet(priv, provider);
 //
 const constants = {
-  proxyAddress: user_addr,
+  proxyAddress: contract_addr,
 };
 
-function toU64ArrayBE(b: BN): Array<bigint>{
-    let bns = new Array<bigint>();
-    let bnStr = b.toString("hex", 64)
-    for (let i = 0; i < bnStr.length; i += 16) {
-        const chunk = bnStr.slice(i, i + 16);
-        let a = BigInt(`0x${chunk}n`);
-        bns.push(a);
+export class NumberUtil {
+    bn: BN;
+    constructor(num: number) {
+        this.bn = new BN(num);
     }
-    return bns;
+    toBN(){
+        let bns = new Array<BN>();
+        let bnStr = this.bn.toString("hex", 64)
+        for (let i = 0; i < bnStr.length; i += 16) {
+            const chunk = bnStr.slice(i, i + 16);
+            let a = new BN(chunk, 'hex', 'be');
+            bns.push(a);
+        }
+        return bns;
+    }
+    toNumber() {
+        return this.toBN().map((x) => x.toString(10));
+    }
 }
 
-async function getMerkle(): Promise<Array<bigint>>{
+async function getMerkle(): Promise<Array<string>>{
   // Connect to the Proxy contract
-  /*
-  const proxy = new ethers.Contract("", provider);
-  const proxy = await ethers.getContractAt("Proxy", constants.proxyAddress, signer);
+  const proxy = new ethers.Contract(constants.proxyAddress, abiData.abi, provider);
   // Fetch the proxy information
   let proxyInfo = await proxy.getProxyInfo();
   console.log("Proxy Info:", proxyInfo);
   // Extract the old Merkle root
   const oldRoot = proxyInfo.merkle_root;
   console.log("Old Merkle Root:", oldRoot);
-  const oldRootU64Array = toU64ArrayBE(oldRoot);
+  let oldRootU64Array = new NumberUtil(oldRoot.toString()).toNumber();
   console.log("Old Merkle Root U64 Array:", oldRootU64Array);
-  */
-  return [0n,0n,0n,0n]
+  return oldRootU64Array;
 }
 
 async function trySettle() {
   let merkle_root = await getMerkle();
+  console.log(merkle_root);
   /*
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
@@ -56,4 +64,4 @@ async function trySettle() {
   */
 }
 
-getMerkle();
+trySettle();
