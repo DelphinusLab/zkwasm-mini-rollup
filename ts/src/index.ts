f26ac5309e2f90152f15d13ff9493e65cf391372
sinka <xgao@zoyoe.com>
2025-01-31 14:21:44 +1100

    export two helper functions of merkleRoot

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,5 +1,6 @@
 import { ZKWasmAppRpc, sign, query, LeHexBN }  from "zkwasm-minirollup-rpc";
 import { composeWithdrawParams} from "./convention.js";
 import { Service } from "./service.js";
+import {hexStringToMerkleRoot, merkleRootToBeHexString} from "./lib.js";
 
-export {sign, query, ZKWasmAppRpc, LeHexBN, composeWithdrawParams, Service}
+export {sign, query, ZKWasmAppRpc, LeHexBN, composeWithdrawParams, Service, merkleRootToBeHexString, hexStringToMerkleRoot}
