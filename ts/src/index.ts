385035e3f39c682a672d57426aec437e7dbe9eeb
Sinka <xgao@zoyoe.com>
2025-02-18 17:19:44 +0800

    export EventModel

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -3,7 +3,7 @@ import { composeWithdrawParams} from "./convention.js";
 import { Service } from "./service.js";
 import { TxWitness } from "./prover.js";
 import { TxStateManager }  from "./commit.js";
-import { Event }  from "./event.js";
+import { Event, EventModel }  from "./event.js";
 import { hexStringToMerkleRoot, merkleRootToBeHexString } from "./lib.js";
 
 export {sign, query, ZKWasmAppRpc, Event, TxWitness, LeHexBN, composeWithdrawParams, Service, merkleRootToBeHexString, hexStringToMerkleRoot, TxStateManager}
