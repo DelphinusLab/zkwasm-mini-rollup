5e8b0926609ddab074c46c5904f5f25efe9683f7
Sinka <xgao@zoyoe.com>
2025-02-18 15:42:19 +0800

    add TxStateManager

---
+++ ts/src/commit.ts
@@ -0,0 +1,107 @@
+import mongoose from 'mongoose';
+import { TxWitness } from './prover.js';
+const txSchema = new mongoose.Schema({
+  msg: { type: String, required: true },
+  pkx: { type: String, required: true },
+  pky: { type: String, required: true },
+  sigx: { type: String, required: true },
+  sigy: { type: String, required: true },
+  sigr: { type: String, required: true },
+});
+
+const commitSchema = new mongoose.Schema({
+    key: { type: String, required: true },
+    items: { type: [txSchema], default: [] } // List of objects
+});
+
+
+export const CommitModel = mongoose.model('Commit', commitSchema);
+
+export class TxStateManager {
+    currentUncommitMerkleRoot: string;
+    uncommittedTxs: TxWitness[];
+    preemptcounter: number;
+
+    constructor(merkleRootHexString: string) {
+      this.currentUncommitMerkleRoot = merkleRootHexString;
+      this.uncommittedTxs = [];
+      this.preemptcounter = 0;
+    }
+
+    async getTxFromCommit(key: string): Promise<TxWitness[]> {
+      try {
+        const commit = await CommitModel.findOne({ key });
+        if (commit) {
+          console.info(`replay uncommitted transactions for commit ${key}: total ${commit.items.length}`);
+          return commit.items.map((x) => {
+            return {
+              msg: x.msg,
+              pkx: x.pkx,
+              pky: x.pky,
+              sigx: x.sigx,
+              sigy: x.sigy,
+              sigr: x.sigr,
+            };
+          });
+        } else {
+          console.info(`non transactions recorded for commit ${key}`);
+          return [];
+        }
+      } catch (error) {
+        console.info(`non transactions recorded for commit ${key}`);
+        return [];
+      }
+    }
+
+    async moveToCommit(key: string) {
+      this.currentUncommitMerkleRoot = key;
+      this.preemptcounter = 0;
+      try {
+        await CommitModel.findOneAndUpdate({
+          key: key
+        }, {
+          key: key,
+          items: []
+        }, {
+          upsert: true
+        });
+      } catch (error) {
+        console.info(`fatal: clear commits should not fail`);
+        process.exit(1);
+      }
+    }
+
+    async insertTxIntoCommit (tx: TxWitness): Promise<boolean>{
+      const counter = this.preemptcounter;
+      const key = this.currentUncommitMerkleRoot;
+      this.preemptcounter += 1;
+      try {
+        const commit = await CommitModel.findOne({ key });
+        console.log(`Inserting tx into bundle with key: ${key}`);
+        if (commit) {
+          // If key exists, push new item to items array
+          if (commit.items.length <= counter) {
+            commit.items.push(tx);
+            await commit.save();
+            return false; // new tx, needs track
+          } else {
+            let trackedTx = commit.items[counter];
+            console.assert(tx == trackedTx);
+            return true; // event already tracked
+          }
+        } else {
+          // If key does not exist, create a new commit record
+          const newCommit = new CommitModel({
+            key,
+            items: [tx], // Insert the new item as the first element
+          });
+          await newCommit.save();
+          return false;
+        }
+      } catch (error) {
+        console.error('Error inserting tx into current bundle:', error);
+        throw (error)
+      }
+    };
+}
+
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,6 +1,8 @@
 import { ZKWasmAppRpc, sign, query, LeHexBN }  from "zkwasm-minirollup-rpc";
 import { composeWithdrawParams} from "./convention.js";
 import { Service } from "./service.js";
-import {hexStringToMerkleRoot, merkleRootToBeHexString} from "./lib.js";
+import { TxWitness } from "./prover.js";
+import { TxStateManager }  from "./commit.js";
+import { hexStringToMerkleRoot, merkleRootToBeHexString } from "./lib.js";
 
-export {sign, query, ZKWasmAppRpc, LeHexBN, composeWithdrawParams, Service, merkleRootToBeHexString, hexStringToMerkleRoot}
+export {sign, query, ZKWasmAppRpc, TxWitness, LeHexBN, composeWithdrawParams, Service, merkleRootToBeHexString, hexStringToMerkleRoot}
