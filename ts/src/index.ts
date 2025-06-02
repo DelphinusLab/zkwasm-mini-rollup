f3fda953dc40bb700c3791cd04ad8077e15488cb
xgao <xgao@zoyoe.com>
2025-06-02 12:36:41 +1000

    export ObjectEvent and PositionEvent

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -5,8 +5,8 @@ import { TxWitness } from "./prover.js";
 import { TxStateManager }  from "./commit.js";
 import { Event, EventModel }  from "./event.js";
 import * as Market from './storage/market.js';
-import * as Object from './storage/object.js';
-import * as Position from './storage/position.js';
+import * as ObjectEvent from './storage/object.js';
+import * as PositionEvent from './storage/position.js';
 import { hexStringToMerkleRoot, merkleRootToBeHexString } from "./lib.js";
 
-export {sign, query, ZKWasmAppRpc, Event, EventModel, TxWitness, LeHexBN, composeWithdrawParams, Service, merkleRootToBeHexString, hexStringToMerkleRoot, TxStateManager, Market}
+export {sign, query, ZKWasmAppRpc, Event, EventModel, TxWitness, LeHexBN, composeWithdrawParams, Service, merkleRootToBeHexString, hexStringToMerkleRoot, TxStateManager, Market, ObjectEvent, PositionEvent}
