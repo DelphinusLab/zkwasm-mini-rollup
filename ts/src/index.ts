81b10785222cf1e3e5580ea649d4867898ae7f3c
xgao <xgao@zoyoe.com>
2025-06-02 12:26:13 +1000

    expose object and position

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -5,6 +5,8 @@ import { TxWitness } from "./prover.js";
 import { TxStateManager }  from "./commit.js";
 import { Event, EventModel }  from "./event.js";
 import * as Market from './storage/market.js';
+import * as Object from './storage/object.js';
+import * as Position from './storage/position.js';
 import { hexStringToMerkleRoot, merkleRootToBeHexString } from "./lib.js";
 
 export {sign, query, ZKWasmAppRpc, Event, EventModel, TxWitness, LeHexBN, composeWithdrawParams, Service, merkleRootToBeHexString, hexStringToMerkleRoot, TxStateManager, Market}
